9a6dcd9465bf354e702e0829f6bcc1fc3fdf6278
Ase444ka <istomina.asia@yandex.ru>
2025-08-27 14:57:46 +0300

    add icons

--- app.vue
+++ app.vue
@@ -1,55 +1,53 @@
 <script lang="ts">
-import '~/assets/css/main.css'
+import '~/assets/css/main.css';
 </script>
 <template>
   <header>
     <h1>Квартиры</h1>
   </header>
-  
+
   <section class="flat-list">
     <div class="flat-list__heading">
-      <div class="flat-list__field-name">
-        Планировка
-      </div>
-      <div class="flat-list__field-name">
-        Квартира
-      </div>
+      <div class="flat-list__field-name">Планировка</div>
+      <div class="flat-list__field-name">Квартира</div>
       <div class="flat-list__sort-by">
         S,м<sup>2</sup>
         <span class="flat-list__order">
+          <SortAscIcon />
+          <SortDescIcon />
         </span>
       </div>
       <div class="flat-list__sort-by">
         Этаж
         <span class="flat-list__order">
+          <SortAscIcon />
+          <SortDescIcon />
         </span>
       </div>
       <div class="flat-list__sort-by">
         Цена, &#8381;
         <span class="flat-list__order">
+          <SortAscIcon />
+          <SortDescIcon />
         </span>
       </div>
     </div>
     <div class="flat-list__item" v-for="n in 5">
       <div class="flat-list__image">
-        <img src="/layouts/1.svg" alt="">
+        <img src="/layouts/1.svg" alt="" />
       </div>
       <div class="flat-list__title">3-комнатная №104</div>
       <div class="flat-list__area">53.1</div>
       <div class="flat-list__floor"><em>2</em> из 17 этаж</div>
-      <div class="flat-list__price">{{new Intl.NumberFormat("ru-RU").format(
-    6650000,
-  )}}</div>
+      <div class="flat-list__price">
+        {{ new Intl.NumberFormat('ru-RU').format(6650000) }}
+      </div>
     </div>
-  </section>  
+  </section>
   <aside class="flat-filters">
-    <div class="flat-filters__radiogroup">
-
-    </div>
+    <div class="flat-filters__radiogroup"></div>
     <div class="flat-filters__control">
-      <div class="flat-filters__label">
-
-      </div>
+      <div class="flat-filters__label"></div>
       <div class="flat-filters__range">
         <div class="flat-filters__limit_min">
           от
@@ -59,13 +57,10 @@ import '~/assets/css/main.css'
           до
           <span class="flat-filters__limit-value"></span>
         </div>
-        
       </div>
     </div>
     <div class="flat-filters__control">
-      <div class="flat-filters__label">
-
-      </div>
+      <div class="flat-filters__label"></div>
       <div class="flat-filters__range">
         <div class="flat-filters__limit_min">
           от
@@ -75,8 +70,7 @@ import '~/assets/css/main.css'
           до
           <span class="flat-filters__limit-value"></span>
         </div>
-  <MySlider />
-
+        <MySlider />
       </div>
     </div>
   </aside>
@@ -94,7 +88,7 @@ h1 {
     padding: 0 0 16px;
     border-bottom: 1px solid rgba(0, 0, 0, 0.1);
     grid-template-columns: 80px 1fr 120px 120px 120px;
-    gap: 20px
+    gap: 20px;
   }
   &__item {
     display: grid;
@@ -103,8 +97,20 @@ h1 {
     padding: 16px 0 24px;
     border-bottom: 1px solid rgba(0, 0, 0, 0.1);
     grid-template-columns: 80px 1fr 120px 120px 120px;
-    gap: 20px
+    gap: 20px;
+  }
+  &__sort-by {
+    display: flex;
+  }
+  &__order {
+    display: flex;
+    flex-direction: column;
+    width: 20px;
+    height: 20px;
+    use {
+      width: 10px;
+      height: 10px;
+    }
   }
 }
-
 </style>
