cdd6e156e5f798eaaf21f393457d0e42df535f55
Ase444ka <istomina.asia@yandex.ru>
2025-08-27 18:42:03 +0300

    style filters, add radiogroup

--- app.vue
+++ app.vue
@@ -36,14 +36,37 @@ import '~/assets/css/main.css';
         </div>
         <div class="flat-list__title">3-комнатная №104</div>
         <div class="flat-list__area">53.1</div>
-        <div class="flat-list__floor"><em>2</em> из 17 этаж</div>
+        <div class="flat-list__floor">2 <em>из 17</em></div>
         <div class="flat-list__price">
           {{ new Intl.NumberFormat('ru-RU').format(6650000) }}
         </div>
       </div>
     </section>
     <aside class="flat-filters">
-      <div class="flat-filters__radiogroup"></div>
+      <div class="flat-filters__radiogroup">
+        <label for="rooms-1" class="radio">
+          <input type="radio" name="rooms" id="rooms-1" class="radio__input" />
+          <div class="radio__control">1К</div>
+        </label>
+        <label for="rooms-2" class="radio">
+          <input type="radio" name="rooms" id="rooms-2" class="radio__input" />
+          <div class="radio__control">1К</div>
+        </label>
+        <label for="rooms-3" class="radio">
+          <input type="radio" name="rooms" id="rooms-3" class="radio__input" />
+          <div class="radio__control">1К</div>
+        </label>
+        <label for="rooms-4" class="radio">
+          <input
+            type="radio"
+            disabled
+            name="rooms"
+            id="rooms-4"
+            class="radio__input"
+          />
+          <div class="radio__control">1К</div>
+        </label>
+      </div>
       <div class="flat-filters__control">
         <div class="flat-filters__label"></div>
         <div class="flat-filters__range">
@@ -109,6 +132,10 @@ h1 {
     border-bottom: 1px solid rgba(0, 0, 0, 0.1);
     grid-template-columns: 80px 1fr 120px 120px 120px;
     gap: 20px;
+    em {
+      color: #848a9b;
+      font-style: normal;
+    }
   }
   &__sort-by {
     display: flex;
@@ -122,6 +149,50 @@ h1 {
       width: 10px;
       height: 10px;
     }
+  }
+}
+.flat-filters {
+  background: linear-gradient(
+    135deg,
+    rgba(174, 228, 178, 0.3) 0%,
+    rgba(149, 208, 161, 0.3) 100%
+  );
+  min-width: 399px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border-radius: 10px;
+  padding: 40px;
+  &__radiogroup {
+    display: flex;
+    gap: 16px;
+  }
+}
+.radio {
+  &__input {
+    position: absolute;
+    clip: rect(0, 0, 0, 0);
+    outline: none;
+    border: none;
+    opacity: 0;
+  }
+  &__control {
+    width: 44px;
+    height: 44px;
+    display: grid;
+    place-items: center;
+    border-radius: 50%;
+    background-color: white;
+    cursor: pointer;
+  }
+  input:checked ~ &__control {
+    background-color: #3eb57c;
+    box-shadow: 0px 6px 20px 0px #95d0a1;
+    color: white;
+  }
+  input[disabled] ~ &__control {
+    color: #ced1d7;
+    cursor: default;
   }
 }
 </style>
